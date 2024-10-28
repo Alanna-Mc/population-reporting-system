bf398e7d9914c1096ba0907e9fe5a9157a52f42d
Alanna Mc <Alanna-Mc@users.noreply.github.com>
2024-10-28 00:59:13 +0000

    Updated name and added example test

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -3,12 +3,12 @@ package com.napier.sem;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class TestApp
 {
     @Test
     void unitTest()
     {
-
+        // Example test with the correct assertion method
+        assertEquals(5, 5, "Messages are equal");
     }
 }
