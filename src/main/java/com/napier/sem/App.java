fba4ebebe586baa1c1c95bc4d0b89c4f7a703b5e
Steven Miller <stevenerm88@gmail.com>
2024-10-29 20:48:54 +0000

    Added code for creating reports.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -32,30 +32,44 @@ public class App
         a.dbHandler.connect();
 
         // Hard coded values
-        int n = 10;
+        int n = 3;
         String continent = "Europe";
         String region = "Eastern Europe";
 
+        // Get all the countries in the world, given continent or given region
+        ArrayList<Country> allGlobalCountries = a.dbHandler.getAllCountries();
+        ArrayList<Country> allContinentCountries = a.dbHandler.getAllCountriesInContinent(continent);
+        ArrayList<Country> allRegionCountries = a.dbHandler.getAllCountriesInRegion(region);
 
         // Get the top N populated countries: globally and by selected continent and region
         ArrayList<Country> globalCountries = a.dbHandler.getTopNPopulatedCountries(n);
-
         ArrayList<Country> continentCountries = a.dbHandler.getTopNPopulatedCountriesInContinent(n, continent);
-
         ArrayList<Country> regionCountries = a.dbHandler.getTopNPopulatedCountriesInRegion(n, region);
 
         // Display each report
+        // display all countries in the world report
+        System.out.println("\nAll Countries in the world:");
+        a.reportsHandler.displayCountries(allGlobalCountries);
+
+        // display all countries  in a given continent report
+        System.out.println("\nAll Countries in " + continent + ":");
+        a.reportsHandler.displayCountries(allContinentCountries);
+
+        // display all countries in a given region report
+        System.out.println("\nAll Countries in " + region + ":");
+        a.reportsHandler.displayCountries(allRegionCountries);
+
         // Get the top N populated countries
         System.out.println("\nTop " + n + " Countries Globally:");
-        a.reportsHandler.displayTopCountries(globalCountries, n);
+        a.reportsHandler.displayCountries(globalCountries);
 
         // Get the top N populated countries by continent
         System.out.println("\nTop " + n + " Countries in " + continent + ":");
-        a.reportsHandler.displayTopCountries(continentCountries, n);
+        a.reportsHandler.displayCountries(continentCountries);
 
         // Get the top N populated countries by region
         System.out.println("\nTop " + n + " Countries in " + region + ":");
-        a.reportsHandler.displayTopCountries(regionCountries, n);
+        a.reportsHandler.displayCountries(regionCountries);
 
         // Disconnect from database
         a.dbHandler.disconnect();
