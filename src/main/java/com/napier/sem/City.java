b0c263671c935c175936eb7998a694a2afc07053
lucas <40646477@live.napier.ac.uk>
2024-11-05 21:49:00 +0000

    add city class

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,36 @@
+package com.napier.sem;
+
+/**
+ * Represents a City
+ */
+public class City {
+    /**
+     * City Name
+     */
+    public static String name;
+
+    /**
+     * City Country
+     */
+    public static String country;
+
+    /**
+     * City District
+     */
+    public static String district;
+
+    /**
+     * City population
+     */
+    public static int population;
+
+    /**
+     * City continent
+     */
+    public static String continent;
+
+    /**
+     * City region
+     */
+    public static String region;
+}
