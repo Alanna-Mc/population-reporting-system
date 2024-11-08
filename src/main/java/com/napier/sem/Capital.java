fa712a3c4e67b163ffd996234f7597d286846c42
Steven Miller <stevenerm88@gmail.com>
2024-11-08 22:56:45 +0000

    Created Capital Class

---
+++ src/main/java/com/napier/sem/Capital.java
@@ -0,0 +1,22 @@
+package com.napier.sem;
+
+/**
+ * Represents a Capital city
+ */
+public class Capital {
+
+    /**
+     * Capital City Name
+     */
+    public String name;
+
+    /**
+     * Country
+     */
+    public String country;
+
+    /**
+     * Population
+     */
+    public int population;
+}
